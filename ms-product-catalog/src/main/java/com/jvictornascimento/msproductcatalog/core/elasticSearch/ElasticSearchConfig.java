b2ade1a764957b806c3dee8cf547177d88385fb2
jvictornascimento <victojoao789@gmail.com>
2025-10-17 16:56:23 -0300

    fix: Change the elasticsearch image and configured

--- ms-product-catalog/src/main/java/com/jvictornascimento/msproductcatalog/core/elasticSearch/ElasticSearchConfig.java
+++ ms-product-catalog/src/main/java/com/jvictornascimento/msproductcatalog/core/elasticSearch/ElasticSearchConfig.java
@@ -1,19 +1,16 @@
-package com.jvictornascimento.msproductcatalog.core.elasticSearch;
+package     com.jvictornascimento.msproductcatalog.core.elasticSearch;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
-@EnableElasticsearchRepositories(basePackages = "com.jvictornascimento.msproductcatalog.domain.repositories")
 public class ElasticSearchConfig extends ElasticsearchConfiguration {
 
     @Override
     public ClientConfiguration clientConfiguration() {
         return ClientConfiguration.builder()
-                .connectedTo("localhost:9200")
+                .connectedTo("elasticsearch:9200")
                 .build();
     }
-
 }
