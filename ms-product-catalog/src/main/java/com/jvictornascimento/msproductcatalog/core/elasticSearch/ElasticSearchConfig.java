ee6153afdba64f59871887707ec7119dffdb1c55
jvictornascimento <victojoao789@gmail.com>
2025-10-16 22:41:05 -0300

    feature: Configured elasticsearch as db

---
+++ ms-product-catalog/src/main/java/com/jvictornascimento/msproductcatalog/core/elasticSearch/ElasticSearchConfig.java
@@ -0,0 +1,19 @@
+package com.jvictornascimento.msproductcatalog.core.elasticSearch;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@Configuration
+@EnableElasticsearchRepositories(basePackages = "com.jvictornascimento.msproductcatalog.domain.repositories")
+public class ElasticSearchConfig extends ElasticsearchConfiguration {
+
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+                .connectedTo("localhost:9200")
+                .build();
+    }
+
+}
