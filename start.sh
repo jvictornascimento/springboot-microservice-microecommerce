3a3781587a13c43cfbc7283a44e65faf9014d632
jvictornascimento <victojoao789@gmail.com>
2025-10-16 21:58:20 -0300

    feature: Created build with docker

--- start.sh
+++ start.sh
@@ -1,5 +1,14 @@
-mvn clean
+#!/bin/bash
 
-mvn package
 
-mvn spring-boot:run
+set -e  # faz o script parar em caso de erro
+
+docker compose -p microservico -f docker-compose.yml up -d
+
+until $(curl -s http://localhost:8888/actuator/health | grep -q '"status":"UP"'); do
+  echo "Aguardando Config Server ficar disponivel"
+  sleep 5
+done
+
+docker compose -p microservico -f ms-user/docker-compose.ms-user.yml up -d
+docker compose -p microservico -f ms-product-catalog/docker-compose.ms-product-catalog.yml up -d
