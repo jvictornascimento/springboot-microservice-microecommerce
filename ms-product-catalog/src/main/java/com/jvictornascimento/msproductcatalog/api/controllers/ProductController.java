7fee2a9d2fd185537c621d9702e4cd659001340a
jvictornascimento <victojoao789@gmail.com>
2025-10-18 18:23:31 -0300

    feature: Added DTO patters to all CRUD methods

--- ms-product-catalog/src/main/java/com/jvictornascimento/msproductcatalog/api/controllers/ProductController.java
+++ ms-product-catalog/src/main/java/com/jvictornascimento/msproductcatalog/api/controllers/ProductController.java
@@ -1,6 +1,8 @@
 package com.jvictornascimento.msproductcatalog.api.controllers;
 
-import com.jvictornascimento.msproductcatalog.domain.models.Product;
+import com.jvictornascimento.msproductcatalog.api.controllers.request.ProductRequest;
+import com.jvictornascimento.msproductcatalog.api.controllers.request.ProductUpdateRequest;
+import com.jvictornascimento.msproductcatalog.api.controllers.response.ProductResponse;
 import com.jvictornascimento.msproductcatalog.domain.service.ProductService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,20 +21,20 @@ public class ProductController {
     }
 
     @GetMapping()
-    public ResponseEntity<List<Product>> getAllProducts() {
+    public ResponseEntity<List<ProductResponse>> getAllProducts() {
         return ResponseEntity.ok(productService.getAll());
     } @GetMapping("/{id}")
-    public ResponseEntity<Product> getProdcutbyId(@PathVariable(name= "id") Long id) {
+    public ResponseEntity<ProductResponse> getProdcutbyId(@PathVariable(name= "id") Long id) {
         return ResponseEntity.ok(productService.getById(id));
     }
     @PostMapping()
-    public ResponseEntity<Product> saveProduct(@RequestBody Product product) {
+    public ResponseEntity<ProductResponse> saveProduct(@RequestBody ProductRequest product) {
         var newData = productService.saveProduct(product);
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newData.getId()).toUri();
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newData.id()).toUri();
         return ResponseEntity.created(uri).body(newData);
     }
     @PutMapping("/{id}")
-    public ResponseEntity<Product> deleteProductById(@PathVariable(name= "id") Long id, @RequestBody Product product) {
+    public ResponseEntity<ProductResponse> deleteProductById(@PathVariable(name= "id") Long id, @RequestBody ProductUpdateRequest product) {
         return ResponseEntity.ok(productService.updateProduct(id, product));
     }  @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteProductById(@PathVariable(name= "id") Long id) {
--- ms-product-catalog/src/main/java/com/jvictornascimento/msproductcatalog/domain/service/ProductService.java
+++ ms-product-catalog/src/main/java/com/jvictornascimento/msproductcatalog/domain/service/ProductService.java
@@ -1,41 +1,48 @@
 package com.jvictornascimento.msproductcatalog.domain.service;
 
+import com.jvictornascimento.msproductcatalog.api.controllers.request.ProductRequest;
+import com.jvictornascimento.msproductcatalog.api.controllers.request.ProductUpdateRequest;
+import com.jvictornascimento.msproductcatalog.api.controllers.response.ProductResponse;
+import com.jvictornascimento.msproductcatalog.api.mapper.IProductMapper;
 import com.jvictornascimento.msproductcatalog.domain.models.Product;
 import com.jvictornascimento.msproductcatalog.domain.repositories.ProductRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductService implements IProductService {
     private final ProductRepository productRepository;
+    private final IProductMapper productMapper;
 
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, IProductMapper productMapper) {
         this.productRepository = productRepository;
+        this.productMapper = productMapper;
     }
 
     @Override
-    public List<Product> getAll() {
+    public List<ProductResponse> getAll() {
         List<Product> list = new ArrayList<>();
-                productRepository.findAll().forEach(list::add);
-        return list;
+        productRepository.findAll().forEach(list::add);
+        return list.stream().map(productMapper::toOut).collect(Collectors.toList());
     }
 
     @Override
-    public Product getById(Long id) {
-        return productRepository.findById(id).get();
+    public ProductResponse getById(Long id) {
+        return productMapper.toOut(productRepository.findById(id).get());
     }
     @Override
-    public Product saveProduct(Product product) {
-        return productRepository.save(product);
+    public ProductResponse saveProduct(ProductRequest product) {
+        return productMapper.toOut(productRepository.save(productMapper.toEntity(product)));
     }
     @Override
-    public Product updateProduct(Long id, Product product) {
-        var data = getById(id);
-        data.setName(product.getName() != null ? product.getName() : data.getName());
-        data.setAmount(product.getAmount() != null ? product.getAmount() : data.getAmount());
-        return productRepository.save(data);
+    public ProductResponse updateProduct(Long id, ProductUpdateRequest product) {
+        var data = productRepository.findById(id).get();
+        data.setName(product.name() != null ? product.name() : data.getName());
+        data.setAmount(product.amount() != null ? product.amount() : data.getAmount());
+        return productMapper.toOut(productRepository.save(data));
     }
     @Override
     public void deleteProduct(Long id) {
